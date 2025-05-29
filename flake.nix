76669b617367cad6cc32593a7db7f832da05ab5e
Gregory Mulvaney <gregtmulvaney@gmail.com>
2025-05-29 16:59:24 -0500

    chore: Added nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Forager flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShell = pkgs.mkShellNoCC {
+          name = "go";
+          buildInputs = with pkgs; [
+            air
+          ];
+        };
+      }
+    );
+}
